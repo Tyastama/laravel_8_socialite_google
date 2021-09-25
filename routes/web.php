9b9b25e2174dcff056a9f8d0fd8ec057aaf69df3
Tama <tyasyanotama@gmail.com>
2021-09-26 01:16:47 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+ 
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+Auth::routes();
+ 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/// arahkan ke link ini ketika user klik "login with google"
+Route::get('auth/google', [App\Http\Controllers\Auth\LoginController::class, 'google']);
+/// siapkan route untuk menampung callback dari google
+Route::get('auth/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'google_callback']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
